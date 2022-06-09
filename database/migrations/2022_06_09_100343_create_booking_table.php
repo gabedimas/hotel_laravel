c3f2ad2fbcafa2caaf36c2a3ff78085ee20cc6a3
Budi Priyo Utomo <budipriyo.utomo@pegadaian.co.id>
2022-06-09 18:12:12 +0700

    tambahan migrate user

--- database/migrations/2022_06_09_100343_create_booking_table.php
+++ database/migrations/2022_06_09_100343_create_booking_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
+        // untuk booking
         Schema::create('booking', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('kamar_id');
+            $table->string('status');
+            $table->datetime('tanggal_awal');
+            $table->datetime('tanggal_akhir');
             $table->timestamps();
         });
     }
--- database/migrations/2022_06_09_100408_create_kamar_table.php
+++ database/migrations/2022_06_09_100408_create_kamar_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('kamar', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('klasifikasi_id');
             $table->timestamps();
         });
     }
